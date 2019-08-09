b967f9913c738d56fd1b28293024df198cfc7859
max-student@outlook.com <max-student@outlook.com>
2019-08-09 15:52:02 -0400

    Added methods

--- Library/Program.cs
+++ Library/Program.cs
@@ -6,32 +6,60 @@ namespace StudentLibrary1 {
     class Program {
         static void Main(string[] args) {
 
-            var about = StudentLib.About;
-            UtilityLibrary.Console.Print(about);
+            //update 'imma' to 'zach' id = 1
+            //var lib = new StudentLib();
+            //var imma = lib.GetStudent(1);
+            //imma.Firstname = "Zach";
+            //var success = lib.UpdateStudent(imma);
 
-            var lib = new StudentLib();
-            var students = lib.ListStudents();
-            foreach (var student in students) {
-                System.Console.WriteLine($"{student.Firstname} {student.Lastname} {student.Major.Description}");
+            //var ef1 = new Student {
+            //    Id = 0,
+            //    Firstname = "Elmer",
+            //    Lastname = "Fudd",
+            //    Sat = 500,
+            //    Gpa = 1.4,
+            //    IsFulltime = true,
+            //    MajorId = 1
+            //};
+            //var ok = lib.InsertStudent(ef1);
 
-            //this should work
-            }
-            var s1 = lib.GetStudent(1);
-            if (s1 == null) {
-                System.Console.WriteLine("Student not found!");
-            }
-            else {
-                System.Console.WriteLine($"S1: {s1.Firstname} {s1.Lastname} {s1.Major.Description}");
-            }
-            //this should fail
-            var s444 = lib.GetStudent(444);
-            if (s444 == null) {
-                System.Console.WriteLine("Student not found!");
-            }
-            else {
-                System.Console.WriteLine($"S1: {s444.Firstname} {s444.Lastname} {s444.Major.Description}");
-            }
+            //System.Console.WriteLine((ok ? "Insert Successful!" : "Insert Failed!"));
+
+
+
+
         }
     }
- }
+    }
+
+
+            //var about = StudentLib.About;
+            //UtilityLibrary.Console.Print(about);
+
+            //var lib = new StudentLib();
+            //var students = lib.ListStudents();
+            //foreach (var student in students) {
+            //    System.Console.WriteLine($"{student.Firstname} {student.Lastname} {student.Major.Description}");
+   
+            //}
+
+            //this should work
+ //           var s1 = lib.GetStudent(1);
+ //           if (s1 == null) {
+ //               System.Console.WriteLine("Student not found!");
+ //           }
+ //           else {
+ //               System.Console.WriteLine($"S1: {s1.Firstname} {s1.Lastname} {s1.Major.Description}");
+ //           }
+ //           //this should fail
+ //           var s444 = lib.GetStudent(444);
+ //           if (s444 == null) {
+ //               System.Console.WriteLine("Student not found!");
+ //           }
+ //           else {
+ //               System.Console.WriteLine($"S1: {s444.Firstname} {s444.Lastname} {s444.Major.Description}");
+ //           }
+ //       }
+ //   }
+ //}
 
--- StudentLibrary1/StudentLib.cs
+++ StudentLibrary1/StudentLib.cs
@@ -10,14 +10,62 @@ namespace StudentLibrary1 {
         public List<Student> ListStudents() {
             var db = new AppEfdbContext();
             return db.Students.ToList();
-
         }
+
         public Student GetStudent(int id) {
             var db = new AppEfdbContext();
             db.Students.Find(id);
             return db.Students.Find(id);
-
         }
 
+        public bool UpdateStudent(Student s) {
+            var db = new AppEfdbContext();
+            var sDB = GetStudent(s.Id);
+            if (sDB == null) {
+                throw new Exception("Student cannot be found!");
+            }
+            sDB.Firstname = s.Firstname;
+            sDB.Lastname = s.Lastname;
+            sDB.Gpa = s.Gpa;
+            sDB.Sat = s.Sat;
+            sDB.IsFulltime = s.IsFulltime;
+            sDB.MajorId = s.MajorId; //see if it exist
+            var major = db.Majors.Find(s.MajorId);
+            if (major == null) {
+                return false;
+            }
+            //or db.Students.Update(sDB);
+            db.Update<Student>(s);
+            db.SaveChanges();
+            return true;
+        }
+
+        public bool DeleteStudent(Student s) {
+            var db = new AppEfdbContext();
+            var sdb = GetStudent(s.Id);
+            if (sdb == null) {
+                throw new Exception("No such student exist!");
+            }
+            db.Remove(s);
+            db.SaveChanges();
+            return true;
+        }
+
+        public bool InsertStudent(Student s) {
+            var db = new AppEfdbContext();
+            s.Id = 0;
+            if (s.MajorId != null) {
+                var major = db.Majors.Find(s.MajorId);
+                if (major == null) {
+                    return false;
+                }
+            }
+                db.Update<Student>(s);
+                db.SaveChanges();
+                return true;
+            }
+
+        }
     }
-}
+
+
