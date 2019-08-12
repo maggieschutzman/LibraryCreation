917f2bace3f54cf169278eaaa096c6c50c249764
max-student@outlook.com <max-student@outlook.com>
2019-08-12 09:20:32 -0400

    Updated

--- StudentLibrary1/StudentLib.cs
+++ StudentLibrary1/StudentLib.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace StudentLibrary1 {
     public class StudentLib {
         public static string About = "About StudentLib";
-
+         
         public List<Student> ListStudents() {
             var db = new AppEfdbContext();
             return db.Students.ToList();
